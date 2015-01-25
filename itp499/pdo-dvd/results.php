9885eac592160ca320443844cf9bab4dab0e888a
Paige Kehoe <pkehoe@usc.edu>
2015-01-24 19:09:12 -0800

    beginning styling on dvd search page

--- itp499/pdo-dvd/results.php
+++ itp499/pdo-dvd/results.php
@@ -1,43 +1,57 @@
 
-/**
- * Created by PhpStorm.
- * User: pkehoe
- * Date: 3/2/14
- * Time: 12:12 PM
- */
 <?php
 
-
+if (!isset($_GET['searchtitle'])) {
+    header('Location:search.php');
+}
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title> <h1>Search Results </h1></title>
+    <title> Search Results</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </head>
-<body>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    You searched for: <?php echo $_GET["searchtitle"]; ?> <br>
+<body>
+<div class="container">
+<h1> Search Results</h1>
+
+    You searched for: <?php echo $_GET["searchtitle"]; ?> </br>
     <?php
         $host = 'itp460.usc.edu';
         $dbname = 'dvd';
         $user = 'student';
         $pass = 'ttrojan';
         $searchtitle = $_GET["searchtitle"];
-        $pdo = new PDO("mysql:host=$host dbname=$dbname", $user, $pass);
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 
-        $sql = "SELECT dvds.title AS title, rating_name, genre_name, format_name
-        FROM dvds WHERE title LIKE '%$serachtitle%'
-        INNER JOIN ratings ON songs.rating_id = ratings.id
-        INNER JOIN genres ON songs.genre_id = genres.id
-        INNER JOIN formats ON formats.format_id = formats.id";
+        $sql = "
+            SELECT title, rating_name, genre_name, format_name
+            FROM dvds
+            INNER JOIN ratings 
+            ON dvds.rating_id = ratings.id
+            INNER JOIN genres 
+            ON dvds.genre_id = genres.id
+            INNER JOIN formats 
+            ON dvd.format_id = formats.id
+            WHERE title LIKE ?
+
+        ";
 
 
         $statement = $pdo->prepare($sql);
+        $like = '%'. $searchtitle .'%';
+        $statement->bindParam(1, $like);
         $statement->execute();
-        $dvds = $statement->fetchAll();
+        $dvds = $statement->fetchAll(PDO::FETCH_OBJ);
 
-        var_dump($dvds);
+        //var_dump($dvds);
 
     echo "<table border='1'>
     <tr>
@@ -50,10 +64,10 @@
     <?php foreach ($dvds as $dvd) : ?>
         <div class = "dvd">
             <h4>
-                <?php echo $dvd->title; ?> by <?php echo $song->artist_name; ?>
+                <?php echo $dvd->title; ?>
             </h4>
             <div> Rating: <?php echo $dvd->rating; ?> </div>
-            <div>Genre: <?php echo $dvd->genre; ?> </div>
+            <div> Genre: <?php echo $dvd->genre; ?> </div>
             <div> Format: <?php echo $dvd->format; ?> </div>
 
 
@@ -61,7 +75,10 @@
         </div>
     <?php endforeach; ?>
 
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 
-
+</div>
 </body>
 </html>
--- itp499/pdo-dvd/search.php
+++ itp499/pdo-dvd/search.php
@@ -1,22 +1,36 @@
 
-/**
- * Created by PhpStorm.
- * User: pkehoe
- * Date: 3/2/14
- * Time: 12:12 PM
- */
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>DVD Seach</title>
+   <link href="css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </head>
-<body>
+ <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <form>
-       Movie Title: <input type="text" name="searchtitle"> <br>
-        <input type = "submit">
-    </form>
-</body>
+
+</br>
+	<body>
+		<div class="container">
+			<div class="jumbotron">
+				<style="background-image: url(../img/jumbotronBackground.png); background-size: 100%;">
+				<h1> DVD Database Search </h1>
+				Input part or all of a DVD title to search for its ratings, genre, and format.
+			</div>
+	    <form action="results.php" method="get">
+	       <h3> Movie Title: <input type="text" name="searchtitle"> 
+	        <input type = "submit"> </h3> 
+	    </form>
+
+	     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	    <!-- Include all compiled plugins (below), or include individual files as needed -->
+	    <script src="js/bootstrap.min.js"></script>
+
+		</div>
+	</body>
 </html>
 
