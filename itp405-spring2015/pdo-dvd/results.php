cc9504867304ef293377ba400f903c3201079dad
Paige Kehoe <pkehoe@usc.edu>
2015-01-25 23:17:51 -0800

    :octocat: bootstraps’ bootstraps

--- itp405-spring2015/pdo-dvd/results.php
+++ itp405-spring2015/pdo-dvd/results.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html>
 
 <?php
 
@@ -6,66 +8,82 @@ if (!isset($_GET['searchtitle'])) {
 }
 
 ?>
-<!DOCTYPE html>
-<html>
+
 <head>
+
     <title> Search Results</title>
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
 </head>
-<meta charset="utf-8">
+
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <body>
-<div class="container">
-<h1> Search Results</h1>
+        <div class="container">
+            <div class="jumbotron">
+                <style="background-image: url(../img/jumbotronBackground.png); background-size: cover;">
+                <h1> DVD Database Search </h1>
+            </div>
+        </div>
 
-    You searched for: <?php echo $_GET["searchtitle"]; ?> </br>
-    <?php
-        $host = 'itp460.usc.edu';
-        $dbname = 'dvd';
-        $user = 'student';
-        $pass = 'ttrojan';
-        $searchtitle = $_GET["searchtitle"];
-        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h1> Search Results</h1>
+            </div>
+        <div class = panel-body>
 
-        $sql = "
-            SELECT title, rating_name, genre_name, format_name
-            FROM dvds
-            INNER JOIN ratings 
-            ON dvds.rating_id = ratings.id
-            INNER JOIN genres 
-            ON dvds.genre_id = genres.id
-            INNER JOIN formats 
-            ON dvd.format_id = formats.id
-            WHERE title LIKE ?
+            <h4>You searched for: </h4> <h3> <?php echo $_GET['searchtitle']; ?> </h3> </br></br>
+        
+        <?php
 
-        ";
+            $host = 'itp460.usc.edu';
+            $dbname = 'dvd';
+            $user = 'student';
+            $pass = 'ttrojan';
+            $searchtitle = $_GET['searchtitle'];
+            $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
+            $sql = "
+                SELECT title, rating_name, genre_name, format_name
+                FROM dvds
+                INNER JOIN ratings 
+                ON dvds.rating_id = ratings.id
+                INNER JOIN genres 
+                ON dvds.genre_id = genres.id
+                INNER JOIN formats 
+                ON dvd.format_id = formats.id
+                WHERE title LIKE ?
+
+            ";
 
 
-        $statement = $pdo->prepare($sql);
-        $like = '%'. $searchtitle .'%';
-        $statement->bindParam(1, $like);
-        $statement->execute();
-        $dvds = $statement->fetchAll(PDO::FETCH_OBJ);
+            $statement = $pdo->prepare($sql);
+            $like = '%'. $searchtitle .'%';
+            $statement->bindParam(1, $like);
+            $statement->execute();
+            $dvds = $statement->fetchAll(PDO::FETCH_OBJ);
 
-        //var_dump($dvds);
+            var_dump($dvds);
 
-    echo "<table border='1'>
-    <tr>
-    <th>Title</th>
-    <th>Rating</th>
-    <th>Genre</th>
-    <th>Format</th>
-    </tr>";
-?>
+        echo "<table border='1'>
+        <tr>
+        <th>Title</th>
+        <th>Rating</th>
+        <th>Genre</th>
+        <th>Format</th>
+        </tr>";
+    ?>
     <?php foreach ($dvds as $dvd) : ?>
         <div class = "dvd">
             <h4>
                 <?php echo $dvd->title; ?>
             </h4>
+            
             <div> Rating: <?php echo $dvd->rating; ?> </div>
             <div> Genre: <?php echo $dvd->genre; ?> </div>
             <div> Format: <?php echo $dvd->format; ?> </div>
--- itp405-spring2015/pdo-dvd/search.php
+++ itp405-spring2015/pdo-dvd/search.php
@@ -4,7 +4,7 @@
 <html>
 <head>
     <title>DVD Seach</title>
-   <link href="css/bootstrap.min.css" rel="stylesheet">
+   <link href="css/bootstrap.css" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </head>
@@ -15,9 +15,22 @@
 
 </br>
 	<body>
+
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+
+		  <div class="navbar-header ">
+          <ul class="nav navbar-nav">
+          	<li><a class="active" href="../search.php"><strong>Paige's Website </strong></a></li>
+            <li><a href="../search.php">Search Page</a></li>
+            <li><a href="#">More to Come!</a></li>
+          </ul>
+        </div>
+		</nav>
+
 		<div class="container">
 			<div class="jumbotron">
-				<style="background-image: url(../img/jumbotronBackground.png); background-size: 100%;">
+				<style="background-image: url(../img/jumbotronBackground.png); background-size: cover;">
 				<h1> DVD Database Search </h1>
 				Input part or all of a DVD title to search for its ratings, genre, and format.
 			</div>
